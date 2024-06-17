7d85c49b7c425fc669c3279ce5f24f20e5615efc
alorme <arthur.lormeauwall@amexgbt.com>
2024-06-17 15:39:42 +0200

    wip

--- setmeup.sh
+++ setmeup.sh
@@ -1,10 +1,12 @@
 mv ~/.zshrc ~/.oldzshrc
 mv ~/.config/tmux/ ~/.config/old-tmux
 mv ~/.config/alacritty ~/.config/old-alacritty
+mv ~/.config/nvim ~/.config/odl-nvim
 
 ln -s ~/dotfiles/config/zshconfig/.zshrc ~/.zshrc
 ln -s ~/dotfiles/config/alacrittyconfig/alacritty  ~/.config/alacritty
 ln -s ~/dotfiles/config/tmuxconfig/tmux ~/.config/tmux
+ln -s ~/dotfiles/config/vimconfig/nvim ~/.config/nvim
 
 #cd ~/
 #mkdir localdotfiles
