5619f392ef4b2dc458338dc781c4447c45453013
alorme <arthur.lormeauwall@amexgbt.com>
2024-06-17 15:35:56 +0200

    wip

--- setmeup.sh
+++ setmeup.sh
@@ -1,9 +1,15 @@
 mv ~/.zshrc ~/.oldzshrc
+mv ~/.config/tmux/ ~/.config/old-tmux
+mv ~/.config/alacritty ~/.config/old-alacritty
+
 ln -s ~/dotfiles/config/zshconfig/.zshrc ~/.zshrc
-cd ~/
-mkdir localdotfiles
-cd localdotfiles
-mkdir config
-touch aliases.sh
-touch env.sh
+ln -s ~/dotfiles/config/alacrittyconfig/alacritty  ~/.config/alacritty
+ln -s ~/dotfiles/config/tmuxconfig/tmux ~/.config/tmux
+
+#cd ~/
+#mkdir localdotfiles
+#cd localdotfiles
+#mkdir config
+#touch aliases.sh
+#touch env.sh
 
