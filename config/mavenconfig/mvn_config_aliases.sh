0cf26fc5b644aec5a6f654ce2ccbc3aef0b185eb
alorme <arthur.lormeauwall@amexgbt.com>
2024-07-29 11:15:51 +0200

    wip

--- config/mavenconfig/mvn_config_aliases.sh
+++ config/mavenconfig/mvn_config_aliases.sh
@@ -8,12 +8,17 @@ alias msbr='mvn spring-boot:run'
 alias mpurgeandcleaninstall='mvn dependency:purge-local-repository -Dinclude:com.googlecode.owasp-java-html-sanitizer -DresolutionFuzziness=groupId -Dverbose ; mvn clean install'
 
 function rsrunintegrationtest {
-  mvn verify -B -f $1 -Plocal -Dit.test=$2 failsafe:integration-test
+  mvn verify -B -f rail-service-infra-parent/rail-service-infra-integration-tests/pom.xml -Plocal -Dit.test=$2 failsafe:integration-test
 }
 
 function rsdebugintegrationtest {
- mvn verify -B -f $1 -Pmaui -Dit.test=$2 failsafe:integration-test -Dmaven.failsafe.debug
+ mvn verify -B -f rail-service-infra-parent/rail-service-infra-integration-tests/pom.xml -Pmaui -Dit.test=$2 failsafe:integration-test -Dmaven.failsafe.debug
 }
+
+function mlogfile {
+  mvn --log-file ./mvn.log clean install
+}
+
 
 function mvnrunut {
   mvn test -Dtest=/"$1#$2/"
--- config/sdkmanconfig/sdkman_config_aliases.sh
+++ config/sdkmanconfig/sdkman_config_aliases.sh
@@ -3,3 +3,4 @@ alias sdkconfigdir='cd $SDKMANCONFIG'
 alias sdkenv='nvim $SDKMANCONFIG/sdkman_config_env.sh'
 
 alias java17="sdk use java 17.0.11-amzn"
+alias java21="sdk use java 21.0.3-amzn"
